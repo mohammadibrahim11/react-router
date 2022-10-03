770a8366fefdaac6955b90cf0a74253e116aefb6
ibrahim <iibrahim01081998@gmail.com>
2022-10-03 07:59:35 +0600

    first commit

---
+++ src/steps.js
@@ -0,0 +1,11 @@
+// import { createBrowserRouter } from "react-router-dom"
+ 
+
+// step-1:first install router by the cmd line:npm install react-router-dom 
+// step-2:creat a router use createBrowserRouter
+// step-3:add router provider and pass router props 
+// step-4:create some Route
+// step-5:making route need two things:{
+//     1.path
+//     // 2.Element
+// // }
